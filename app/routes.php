c460db10d3a0abe87cf505dce6561709867409bd
carolineadicahya <11211026@student.itk.ac.id>
2023-10-25 13:49:03 +0800

    get select all dari semua tabel

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,52 @@ use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    // tabel detail
-    //get
+    // GET = SELECT
+    // tabel barang
+    $app->get('/barang', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query("SELECT * FROM barang"); //query digunakan agar langsung execute
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($result));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel supplier
+    $app->get('/supplier', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query("SELECT * FROM supplier"); //query digunakan agar langsung execute
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($result));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel reseller
+    $app->get('/reseller', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query("SELECT * FROM reseller"); //query digunakan agar langsung execute
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($result));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel memesan
+    $app->get('/memesan', function(Request $request, Response $response) {
+        $db = $this->get(PDO::class);
+
+        $query = $db->query("SELECT * FROM memesan"); //query digunakan agar langsung execute
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        $response->getBody()->write(json_encode($result));
+
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel detail_transaksi
     $app->get('/detail_pesanan', function(Request $request, Response $response) {
         $db = $this->get(PDO::class);
 
@@ -22,7 +66,41 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // get untuk satu data, by id
+    // GET by Id = SELECT by Id
+    // tabel barang
+    $app->get('/barang/{id_barang}', function(Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $id = $args['id_barang'];
+    
+        // Menyiapkan SQL untuk memanggil procedure GetBarang
+        $sql = "CALL GetBarang(:id_barang)";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id_barang', $id, PDO::PARAM_STR);
+    
+        // Jalankan query
+        if ($stmt->execute()) {
+            // Mengambil hasil dari procedure
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if ($result) {
+                $response->getBody()->write(json_encode($result[0]));
+            } else {
+                // Jika data tidak ditemukan, kirim respons dengan status 404
+                $response->getBody()->write(json_encode(['error' => 'Data pabrik tidak ditemukan']));
+                $response = $response->withStatus(404);
+            }
+        } else {
+            // Tangani kesalahan eksekusi query
+            $errorInfo = $stmt->errorInfo();
+            $response->getBody()->write(json_encode(['error' => $errorInfo]));
+            $response = $response->withStatus(500); // Atur status kode 500 untuk kesalahan server
+        }
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel supplier
+    // tabel reseller
+    // tabel memesan
+    // tabel detail_transaksi
     $app->get('/detail_pesanan/{id}', function(Request $request, Response $response, $args) {
         $db = $this->get(PDO::class);
         $id = $args['id'];
@@ -51,11 +129,51 @@ return function (App $app) {
         }
         return $response->withHeader("Content-Type", "application/json");
     });
-    
 
-    // Post
-    $app->post('/detail_pesanan', function(Request $request, Response $response) {
+    // POST = INSERT
+    // tabel barang
+    $app->post('/barang', function(Request $request, Response $response) {
         $parsedBody = $request->getParsedBody();
+    
+        $idBarang = $parsedBody['barang_id'];
+        $Nama = $parsedBody['barang_nama'];
+        $Brand = $parsedBody['barang_brand'];
+        $Model = $parsedBody['barang_model'];
+        $Jumlah = $parsedBody['barang_jumlah'];
+    
+        $db = $this->get(PDO::class);
+    
+        try {
+            $query = $db->prepare('CALL AddBarang(?, ?, ?, ?, ?)');
+            $query->bindParam(1, $idBarang, PDO::PARAM_STR);
+            $query->bindParam(2, $Nama, PDO::PARAM_STR);
+            $query->bindParam(3, $Brand, PDO::PARAM_STR);
+            $query->bindParam(4, $Model, PDO::PARAM_STR);
+            $query->bindParam(5, $Jumlah, PDO::PARAM_INT);
+    
+            $query->execute();
+    
+            $response->getBody()->write(json_encode(
+                [
+                    'message' => 'Detail barang disimpan dengan id ' . $idBarang
+                    ]
+            ));
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Gagal menyimpan detail pesanan: ' . $e->getMessage()
+                ]
+            ));
+        }
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel supplier
+    // tabel reseller
+    // tabel memesan
+    // tabel detail_transaksi
+    $app->post('/detail_pesanan', function(Request $request, Response $response) {
+        $parsedBody = $request->getParsedBody;
     
         $idDetail = $parsedBody['id_detail'];
         $idBarang = $parsedBody['id_barang'];
@@ -90,7 +208,56 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // put
+
+    // PUT = UPDATE
+    // tabel barang
+    $app->put('/barang/{barang_id}', function(Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $id = $args['barang_id']; // Menggunakan 'id_barang' sesuai dengan yang Anda definisikan dalam rute
+        $parsedBody = $request->getParsedBody();
+        
+        $newNama = isset($parsedBody['new_nama']) ? $parsedBody['new_nama'] : null;
+        $newBrand = isset($parsedBody['new_brand']) ? $parsedBody['new_brand'] : null;
+        $newModel = isset($parsedBody['new_model']) ? $parsedBody['new_model'] : null;
+        $newJumlah = isset($parsedBody['new_jumlah']) ? $parsedBody['new_jumlah'] : null;
+    
+        if ($newNama === null && $newBrand === null && $newModel === null && $newJumlah === null) {
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Tidak ada data yang diperbarui.'
+                ]
+            ));
+            return $response->withStatus(400); // Atur status kode ke 400 Bad Request atau sesuai kebutuhan
+        }
+        try {
+            $query = $db->prepare('CALL UpdateBarang(?, ?, ?, ?, ?)');
+            $query->bindParam(1, $id, PDO::PARAM_STR);
+            $query->bindParam(2, $newNama, PDO::PARAM_STR);
+            $query->bindParam(3, $newBrand, PDO::PARAM_STR);
+            $query->bindParam(4, $newModel, PDO::PARAM_STR); // Ganti PDO::PARAM_INT menjadi PDO::PARAM_STR karena 'new_model' adalah VARCHAR
+            $query->bindParam(5, $newJumlah, PDO::PARAM_INT);
+        
+            $query->execute();
+        
+            $response->getBody()->write(json_encode(
+                [
+                    'message' => 'Detail barang dengan id ' . $id . ' telah diperbarui'
+                ]
+            ));
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Gagal memperbarui barang: ' . $e->getMessage()
+                ]
+            ));
+        }
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel supplier
+    // tabel reseller
+    // tabel memesan
+    // tabel detail_transaksi
     $app->put('/detail_pesanan/{id}', function(Request $request, Response $response, $args) {
         $db = $this->get(PDO::class);
         $id = $args['id'];
@@ -126,7 +293,38 @@ return function (App $app) {
         return $response->withHeader("Content-Type", "application/json");
     });
 
-    // delete
+
+    // DELETE = DELETE
+    // tabel barang
+    $app->delete('/barang/{id_barang}', function(Request $request, Response $response, $args) {
+        $db = $this->get(PDO::class);
+        $id = $args['id_barang'];
+    
+        try {
+            $query = $db->prepare('CALL DeleteBarang(?)');
+            $query->bindParam(1, $id, PDO::PARAM_STR);
+    
+            $query->execute();
+    
+            $response->getBody()->write(json_encode(
+                [
+                    'message' => 'Detail pesanan dengan id ' . $id . ' telah dihapus'
+                ]
+            ));
+        } catch (PDOException $e) {
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => 'Gagal menghapus barang: ' . $e->getMessage()
+                ]
+            ));
+        }
+        return $response->withHeader("Content-Type", "application/json");
+    });
+
+    // tabel supplier
+    // tabel reseller
+    // tabel memesan
+    // tabel detail_transaksi
     $app->delete('/detail_pesanan/{id}', function(Request $request, Response $response, $args) {
         $db = $this->get(PDO::class);
         $id = $args['id'];
@@ -152,19 +350,5 @@ return function (App $app) {
     
         return $response->withHeader("Content-Type", "application/json");
     });
-    
-    
-    // Barang
-    $app->get('/barang', function(Request $request, Response $response) {
-        $db = $this->get(PDO::class);
 
-        $query = $db->query("SELECT * FROM detail_pesanan"); //query digunakan agar langsung execute
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
-        $response->getBody()->write(json_encode($result));
-
-        return $response->withHeader("Content-Type", "application/json");
-    });
-
-    // get untuk satu data, by id
-    
 };
